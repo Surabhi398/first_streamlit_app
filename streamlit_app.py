fbbda6e20050237fe2902275d6f6e75cfc2d95dd
Surabhi398 <156123239+Surabhi398@users.noreply.github.com>
2024-01-12 03:36:57 +0530

    Update streamlit_app.py
    created header
    created content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 And Blueberry Omlet')
+streamlit.text('Banana Pancakes')
+streamlit.text('Egg Sandwich')
